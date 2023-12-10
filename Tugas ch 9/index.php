ec19922bac3b223e97f549f77572a69caf4484ad
akhrf <ahmadkarta9@gmail.com>
2023-12-10 20:08:03 +0700

    baru

---
+++ Tugas ch 9/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran mahasiswa baru</title>
+</head>
+<body>
+    <header>
+        <h3>Pendaftaran Mahasiswa Baru</h3>
+        <h1>Mahasiswa Kami</h1>
+
+        <h4>Menu</h4>
+        <nav>
+            <ul>
+                <li><a href="form-daftar.php"></a>Daftar</li>
+                <li><a href="list-maba.php"></a>Pendaftar</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
